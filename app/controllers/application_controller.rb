5b91127819b24c0533ae22bcc91dd66147f06a25
dan <dan.cristian.bota@gmail.com>
2020-07-26 19:59:30 +0300

    226. Task 7: Start authentication system + auth helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  def current_user
+    User.find_by(session[:user_id]) if logged_in?
+  end
+
+  def require_user
+    return if logged_in?
+
+    flash[:alert] = 'You need to be logged in to perform this action!'
+    redirect_to login_path
+  end
 end
